2d8599f43292321eab71db81e1dc16e6f2d52ec0
Justin Rubek <rubejus@bvu.edu>
2018-01-31 01:08:27 -0600

    Removed custom semanticui theme references

--- js/index.jsx
+++ js/index.jsx
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-import '../semantic/dist/semantic.css'
-
 ReactDOM.render(<App />, document.getElementById("content"));
